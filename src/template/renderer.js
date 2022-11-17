0d97ac12bd79436eb49a0cfeaa98c92e2731922d
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-11-17 20:54:37 +0100

    improve devex

--- src/template/renderer.js
+++ src/template/renderer.js
@@ -3,14 +3,26 @@ const path = require("path");
 
 const { writeZipCodeFile, writeInDist } = require("../file");
 
+const defaultPayload = {
+  appName: "Rapports DVF",
+};
+
 async function renderTemplate(template, payload = {}) {
-  const html = await ejs.renderFile(path.join(__dirname, template), payload, { beautify: false, rmWhitespace: true });
+  const html = await ejs.renderFile(
+    path.join(__dirname, template),
+    { ...defaultPayload, ...payload },
+    { beautify: false, rmWhitespace: true }
+  );
 
   await writeZipCodeFile(payload.zipCode, "index.html", html);
 }
 
 async function renderHomeTemplate(payload = {}) {
-  const html = await ejs.renderFile(path.join(__dirname, "home.ejs"), payload, { beautify: false, rmWhitespace: true });
+  const html = await ejs.renderFile(
+    path.join(__dirname, "home.ejs"),
+    { ...defaultPayload, ...payload },
+    { beautify: false, rmWhitespace: true }
+  );
 
   await writeInDist("index.html", html);
 }
